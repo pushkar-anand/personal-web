67efdaa38266b4df8979129ff990f9dcb8d15be7
Pushkar Anand <anandpushkar088@gmail.com>
2017-07-30 14:41:40 +0530

    added loadjs

---
+++ html/js/loadjs.php
@@ -0,0 +1,31 @@
+<?php
+require_once '../vendor/autoload.php';
+function test_input($data)
+{
+	$data = trim($data);
+  	$data = stripslashes($data);
+  	$data = htmlspecialchars($data);
+  	$data = strip_tags($data);
+  	return $data;
+}
+if( isset($_GET['js']))
+{
+	use MatthiasMullie\Minify;
+	$jsF = test_input($_GET['js']);
+	$jsFiles = explode('/', $jsF);
+
+	$buffer = "";
+
+	$minifier = new Minify\JS();
+	foreach ($jsFiles as $jsFile)
+	{
+		if(!file_exists($jsFile))
+		{
+			error_log($jsFile);
+			header("HTTP/1.0 404 Not Found");
+			exit();
+		}
+		$minifier->add($jsFile);
+	}
+	echo $minifier->minify();
+}
