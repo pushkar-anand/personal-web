5e238421719ce5af1da8d796ad63c2c4266d18fa
Pushkar Anand <anandpushkar088@gmail.com>
2017-08-13 23:36:44 +0530

    added some code

--- html/project.php
+++ html/project.php
@@ -3,6 +3,11 @@ if(isset($_GET['list']))
 {
   $jsonFile = "project.json";
   $projectData = file_get_contents($jsonFile);
+  $jsonArr = json_decode($projectData, true);
+
+  foreach ($variable as $key => $value) {
+    # code...
+  }
 
 }
 else
