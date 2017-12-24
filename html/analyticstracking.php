b140349f92ea0ea3a207bc7983cc07e59db81d18
Pushkar Anand <anandpushkar088@gmail.com>
2017-12-24 21:45:09 +0530

    added fb analytics

--- html/analyticstracking.php
+++ html/analyticstracking.php
@@ -8,3 +8,24 @@
   ga('send', 'pageview');
 
 </script>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '168847740388497',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.11'
+    });
+
+    FB.AppEvents.logPageView();
+
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
