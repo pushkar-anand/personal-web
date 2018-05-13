0429c121bfe2dba4de2b4918bc29ddf2705cca67
Pushkar Anand <anandpushkar088@gmail.com>
2018-05-13 22:50:20 +0530

    added mysql caching

--- html/apps/copy-mean/word-of-the-day.php
+++ html/apps/copy-mean/word-of-the-day.php
@@ -1,5 +1,8 @@
 <?php
+require_once __DIR__."/../../../includes/app.db.config.php";
+
 header('Content-Type: application/json');
+
 $url = "https://www.merriam-webster.com/word-of-the-day";
 
 $user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
@@ -8,9 +11,28 @@ $todays_date = date("d-m-y");
 $filename = $todays_date.'.html';
 $file_full_path = __DIR__.'/'.$filename;
 
-if(!file_exists($file_full_path))
+function entry_exists(string $date):bool
 {
-	error_log("$file_full_path does not exists\n") ;
+    global $conn;
+
+    $q = "SELECT word_id FROM copy_mean_daily_word WHERE php_date = ?";
+    $stmt = $conn->prepare($q);
+
+    $stmt->bind_param("s",$date);
+    $stmt->execute();
+
+    $stmt->store_result();
+    if($stmt->num_rows>0)
+    {
+        return true;
+    }
+    return false;
+}
+
+
+if(!entry_exists($todays_date))
+{
+	error_log("$todays_date does not exists\n") ;
 
 	$options = array(
   'http'=>array(
@@ -23,46 +45,51 @@ if(!file_exists($file_full_path))
 	$context = stream_context_create($options);
 	$html = file_get_contents($url, false, $context);
 
-	$file = fopen($file_full_path, "w");
-	fwrite($file, $html);
-	fclose($file);
+    $doc = new DomDocument();
+    @$doc->loadHTML($html);
 
-	$yestrdy = date("d-m-y", strtotime("yesterday"));
-	$yfile = $yestrdy.'.html';
-	if(file_exists($yfile))
-	{
-		unlink($yfile);
-	}
+    $xpath = new DomXPath($doc);
+
+    $wordXPath = "/html/body/div[1]/div/div[4]/main/article/div[1]/div[2]/div[1]/div/h1";
+    $word = $xpath->query($wordXPath);
+
+    $definationXPath = "/html/body/div[1]/div/div[4]/main/article/div[3]/div[1]/div/div[1]/p";
+    $defination = $xpath->query($definationXPath);
+
+    $exampleXPath = "/html/body/div[1]/div/div[4]/main/article/div[3]/div[1]/div/div[2]/p[1]";
+    $example = $xpath->query($exampleXPath);
+
+    /*var_dump($word[0]->nodeValue);
+    var_dump($defination[0]->nodeValue);
+    var_dump($example[0]->nodeValue);*/
+
+
+    $word = $word[0]->nodeValue;
+    $meaning = $defination[0]->nodeValue;
+    $example = $example[0]->nodeValue;
+
+    $insertQuery = "INSERT INTO copy_mean_daily_word (word, meaning, example, php_date) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($insertQuery);
+
+    $stmt->bind_param("ssss",$word,$meaning, $example, $todays_date);
+    $stmt->execute();
+
+
+
 }
 else
 {
-	error_log("$file_full_path exists\n");
-	$html = file_get_contents($file_full_path);
+	error_log("$todays_date exists\n");
+
+    $selectQuery = "SELECT word, meaning, example FROM copy_mean_daily_word WHERE php_date = ?";
+    $stmt = $conn->prepare($selectQuery);
+
+    $stmt->bind_param("s",$todays_date);
+    $stmt->execute();
+
+    $stmt->bind_result( $word,$meaning, $example);
+    $stmt->fetch();
 }
-
-$doc = new DomDocument();
-@$doc->loadHTML($html);
-
-$xpath = new DomXPath($doc);
-
-$wordXPath = "/html/body/div[1]/div/div[4]/main/article/div[1]/div[2]/div[1]/div/h1";
-$word = $xpath->query($wordXPath);
-
-$definationXPath = "/html/body/div[1]/div/div[4]/main/article/div[3]/div[1]/div/div[1]/p";
-$defination = $xpath->query($definationXPath);
-
-$exampleXPath = "/html/body/div[1]/div/div[4]/main/article/div[3]/div[1]/div/div[2]/p[1]";
-$example = $xpath->query($exampleXPath);
-
-/*var_dump($word[0]->nodeValue);
-var_dump($defination[0]->nodeValue);
-var_dump($example[0]->nodeValue);*/
-
-
-$word = $word[0]->nodeValue;
-$meaning = $defination[0]->nodeValue;
-$example = $example[0]->nodeValue;
-
 
 $output = array('word' => $word, 'mean' => $meaning, 'example' => $example );
 $outputJSON = json_encode($output);
